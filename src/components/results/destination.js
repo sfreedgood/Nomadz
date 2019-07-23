8acd61c1bc564853645b8ddc4bcae5aa4bd9a836
samfreedgood <sam.freedgood@gmail.com>
2019-07-23 00:01:01 -0400

    prep for router implementation

--- src/components/results/destination.js
+++ src/components/results/destination.js
@@ -1,12 +1,12 @@
-import React from "react"
+// import React from "react"
 
-function Destination (props) {
+// function Destination (props) {
 
-  return(
-    <div className="destination">
-      I'm destination {props.destination}
-    </div>
-  )
-}
+//   return(
+//     <div className="destination">
+//       I'm destination {props.destination}
+//     </div>
+//   )
+// }
 
-export default Destination
+// export default Destination
